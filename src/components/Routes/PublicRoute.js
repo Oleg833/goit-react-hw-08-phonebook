b807a5a9188bf3606f9a5c3b1ce32c65bb3b8fe2
Oleg833 <oleg.kovtun447@gmail.com>
2023-05-29 10:36:38 +0300

    ContactsPage

--- src/components/Routes/PublicRoute.js
+++ src/components/Routes/PublicRoute.js
@@ -1,10 +1,11 @@
-const { Navigate } = require('react-router-dom');
+import ContactsPage from 'pages/ContactsPage';
+
 const { useSelector } = require('react-redux');
 const { selectAuth } = require('redux/auth/selectors');
 
 const PublicRoute = ({ children }) => {
   const { isLoggedIn } = useSelector(selectAuth);
-  return !isLoggedIn ? children : <Navigate to="/" />;
+  return !isLoggedIn ? children : <ContactsPage />;
 };
 
 export default PublicRoute;
